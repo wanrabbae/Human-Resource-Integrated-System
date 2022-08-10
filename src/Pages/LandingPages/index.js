f440fce7fb8e2cfbbddf0a826b9cb9627efb0a50
mfzall07 <mfaisalhafid@gmail.com>
2022-08-10 14:39:07 +0700

    first commit

---
+++ src/Pages/LandingPages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPages() {
+  return (
+    <div>
+        <h1 className='text-2xl text-cyan-500 font-bold p-5'>Hello Wordl!</h1>
+    </div>
+  )
+}
+
+export default LandingPages

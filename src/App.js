5bdc8cc61289ed87cd7e8be760ac45c082c00af7
farishasyim <faris.hasyim.03@gmail.com>
2022-08-12 13:02:39 +0700

    adding template admin

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import LandingPages from './Pages/LandingPages';
 import Dashboard from './Pages/Dashboard';
+import Template from './Pages/Administrator/template';
+import Home from './Pages/Administrator/Admin/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPages />}></Route>
       <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="/administration" element={<Template />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }

de694be4c1fa19975296283ec8427452dfca2ff4
Puspa Wahyuningtias <puspa.wahyuningtias@gmail.com>
2022-08-20 13:58:44 +0700

    Update Navbar.js

--- src/Components/landingPages/Navbar.js
+++ src/Components/landingPages/Navbar.js
@@ -41,7 +41,7 @@ function navbar() {
             <ul className="flex flex-col p-2 mt-2 bg-gray-50 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   aria-current="page"
                 >
@@ -50,7 +50,7 @@ function navbar() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/feature"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Features
@@ -58,7 +58,7 @@ function navbar() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/pricing"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Pricing

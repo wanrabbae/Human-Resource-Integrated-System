15e50aa30ab5cd1da45da71630577e0f814521a2
nivlaw <ojodumeh337@gmail.com>
2022-11-11 10:15:14 +0700

    fixing add emmployee

--- src/Pages/Administrator/Employee/Configuration/EmployeeList.js
+++ src/Pages/Administrator/Employee/Configuration/EmployeeList.js
@@ -618,6 +618,46 @@ function EmployeeList() {
             Add Employee
           </Modal.Title>
         </Modal.Header>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            var formData = new FormData();
+            formData.append("image", controller.image);
+            formData.append("firstName", controller.firstName);
+            formData.append("lastName", "");
+            formData.append("employeeId", "GENERATE001");
+            formData.append("joinedDate", controller.joinDate);
+            formData.append("jobtitle_id", controller.jobtitle_id);
+            formData.append(
+              "employeestatus_id",
+              controller.employeestatus_id
+              );
+              formData.append("jobgrade_id", controller.jobgrade_id);
+            formData.append("joblevel_id", controller.joblevel_id);
+            formData.append("jobposition_id", controller.jobposition_id);
+            formData.append("location", controller.location);
+            var data = await AddEmployee(formData);
+            if (show) {
+              await AddUser({
+                role: "user",
+                employee_id: data.data.id, //BELUM
+                status: controller.status,
+                username: controller.username,
+                password: controller.password,
+                location: controller.location,
+              });
+              console.log("SUCCESS CREATE USER");
+            }
+
+            if (data.message == "Success") {
+              await inAwait();
+              setModalAdd(false);
+              setController({});
+              SwalSuccess({ message: "Success add employee" });
+              setProfilePict(null);
+            }
+          }}
+        >
         <Modal.Body className="mx-4">
           <div>
             <label htmlFor="imagePicker">
@@ -650,10 +690,11 @@ function EmployeeList() {
                   setController({ ...controller, firstName: val.target.value });
                 }}
                 value={controller?.firstName}
+                required
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="text"
                 placeholder="First Name"
-              />
+                />
             </div>
             <div className="w-full">
               <label className="text-xs"></label>
@@ -661,8 +702,9 @@ function EmployeeList() {
                 onChange={(val) => { }}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="text"
+                required
                 placeholder="Last Name"
-              />
+                />
             </div>
             <div className="w-full">
               <label className="text-xs">Employee ID</label>
@@ -678,6 +720,7 @@ function EmployeeList() {
             <div className="w-full">
               <label className="text-xs">Joined Date</label>
               <input
+                required
                 onChange={(val) => {
                   setController({ ...controller, joinDate: val.target.value });
                 }}
@@ -685,11 +728,12 @@ function EmployeeList() {
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="date"
                 placeholder=""
-              />
+                />
             </div>
             <div className="w-full">
               <label className="text-xs">Employee Status</label>
               <select
+                required
                 onChange={(val) =>
                   setController({
                     ...controller,
@@ -698,7 +742,7 @@ function EmployeeList() {
                 }
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
-                <option hidden>--Select--</option>
+                <option hidden value="">--Select--</option>
                 {employeeStatus.map((e, i) => {
                   return <option value={e.id}>{e.name}</option>;
                 })}
@@ -707,6 +751,7 @@ function EmployeeList() {
             <div className="w-full">
               <label className="text-xs">Job Grade</label>
               <select
+                required
                 onChange={(val) =>
                   setController({
                     ...controller,
@@ -714,8 +759,8 @@ function EmployeeList() {
                   })
                 }
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                <option hidden>--Select--</option>
+                >
+                <option hidden value="">--Select--</option>
                 {jobGrade.map((e, i) => {
                   return <option value={e.id}>{e.name}</option>;
                 })}
@@ -724,6 +769,7 @@ function EmployeeList() {
             <div className="w-full">
               <label className="text-xs">Job Level</label>
               <select
+                required
                 onChange={(val) =>
                   setController({
                     ...controller,
@@ -731,8 +777,8 @@ function EmployeeList() {
                   })
                 }
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                <option hidden>--Select--</option>
+                >
+                <option hidden value="">--Select--</option>
                 {jobLevel.map((e, i) => {
                   return <option value={e.id}>{e.name}</option>;
                 })}
@@ -741,6 +787,7 @@ function EmployeeList() {
             <div className="w-full">
               <label className="text-xs">Job Title</label>
               <select
+                required
                 onChange={(val) =>
                   setController({
                     ...controller,
@@ -749,7 +796,7 @@ function EmployeeList() {
                 }
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
-                <option hidden>--Select--</option>
+                <option hidden value="">--Select--</option>
                 {jobTitle.map((e, i) => {
                   return <option value={e.id}>{e.name}</option>;
                 })}
@@ -758,6 +805,7 @@ function EmployeeList() {
             <div className="w-full">
               <label className="text-xs">Job Position</label>
               <select
+                required
                 onChange={(val) =>
                   setController({
                     ...controller,
@@ -765,8 +813,8 @@ function EmployeeList() {
                   })
                 }
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                <option hidden>--Select--</option>
+                >
+                <option hidden value="">--Select--</option>
                 {jobPosition.map((e, i) => {
                   return <option value={e.id}>{e?.name}</option>;
                 })}
@@ -776,13 +824,14 @@ function EmployeeList() {
             <div className="w-full">
               <label className="text-xs">Location</label>
               <input
+                required
                 onChange={(val) =>
                   setController({ ...controller, location: val.target.value })
                 }
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="text"
                 placeholder="ex : California"
-              />
+                />
             </div>
           </div>
           <div className="mt-3">
@@ -805,7 +854,7 @@ function EmployeeList() {
                     status: null,
                   });
                 }}
-              />
+                />
               <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
               <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Create Login Details
@@ -818,6 +867,7 @@ function EmployeeList() {
                 <div className="w-full">
                   <label className="text-xs">Username</label>
                   <input
+                    required={show}
                     value={controller.username}
                     onChange={(val) =>
                       setController({
@@ -828,13 +878,14 @@ function EmployeeList() {
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     placeholder=""
-                  />
+                    />
                 </div>
               </div>
               <div className="grid grid-cols-2 gap-3 mt-3">
                 <div className="w-full">
                   <label className="text-xs">Password</label>
                   <input
+                    required={show}
                     value={controller.password}
                     onChange={(val) =>
                       setController({
@@ -850,6 +901,7 @@ function EmployeeList() {
                 <div className="w-full">
                   <label className="text-xs">Confirm Password</label>
                   <input
+                    required={show}
                     value={controller.confirm}
                     onChange={(val) =>
                       setController({
@@ -879,7 +931,7 @@ function EmployeeList() {
                       }
                       name="default-radio"
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
+                      />
                     <label
                       for="default-radio-1"
                       className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -900,7 +952,7 @@ function EmployeeList() {
                       value={"Disable"}
                       name="default-radio"
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
+                      />
                     <label
                       for="default-radio-2"
                       className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -922,53 +974,17 @@ function EmployeeList() {
             }}
             type="button"
             className="text-[#003049] bg-gray-200 hover:bg-gray-300 font-sm rounded-lg text-sm px-4 py-2.5 mr-2 mb-2 dark:bg-gray-200 dark:hover:bg-gray-300 focus:outline-none"
-          >
+            >
             Cancel
           </button>
           <button
-            onClick={async () => {
-              var formData = new FormData();
-              formData.append("image", controller.image);
-              formData.append("firstName", controller.firstName);
-              formData.append("lastName", "");
-              formData.append("employeeId", "GENERATE001");
-              formData.append("joinedDate", controller.joinDate);
-              formData.append("jobtitle_id", controller.jobtitle_id);
-              formData.append(
-                "employeestatus_id",
-                controller.employeestatus_id
-              );
-              formData.append("jobgrade_id", controller.jobgrade_id);
-              formData.append("joblevel_id", controller.joblevel_id);
-              formData.append("jobposition_id", controller.jobposition_id);
-              formData.append("location", controller.location);
-              var data = await AddEmployee(formData);
-              if (show) {
-                await AddUser({
-                  role: "user",
-                  employee_id: data.data.id, //BELUM
-                  status: controller.status,
-                  username: controller.username,
-                  password: controller.password,
-                  location: controller.location,
-                });
-                console.log("SUCCESS CREATE USER");
-              }
-
-              if (data.message == "Success") {
-                await inAwait();
-                setModalAdd(false);
-                setController({});
-                SwalSuccess({ message: "Success add employee" });
-                setProfilePict(null);
-              }
-            }}
-            type="button"
+            type="submit"
             className="text-white bg-[#0E5073] hover:bg-[#003049] font-sm rounded-lg text-sm px-4 py-2.5 mr-2 mb-2 dark:bg-[#0E5073] dark:hover:bg-[#003049] focus:outline-none"
-          >
+            >
             Create
           </button>
         </Modal.Footer>
+        </form>
       </Modal>
 
       {/* Modal Update */}

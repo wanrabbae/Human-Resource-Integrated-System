dd8133ae6ed0aa1de30c879a6267b85674462c47
farishasyim <faris.hasyim.03@gmail.com>
2022-11-14 09:39:15 +0700

    handle when nationality is not selected

--- src/Pages/Administrator/ProfileEmployee/Components/personal_detail.js
+++ src/Pages/Administrator/ProfileEmployee/Components/personal_detail.js
@@ -13,7 +13,7 @@ import { useLocation } from "react-router-dom";
 
 function PersonalDetail() {
   const [nationalities, setNationalities] = useState([]);
-  const [selectedNationalities, setSelectedNationalities] = useState([]);
+  const [selectedNationalities, setSelectedNationalities] = useState(null);
   const [profile, setProfile] = useState([]);
   const [firstName, setFirstName] = useState([]);
   const [lastName, setLastName] = useState([]);
